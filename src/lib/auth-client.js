879c9b0ff8772377a1fa7af73429537dd4a7c653
crack69-git <tanchangyaashutosh0356@gmail.com>
2026-05-09 21:04:28 +0600

    some adjustment

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,5 +1,14 @@
 import { createAuthClient } from "better-auth/react";
+
+const getBaseURL = () => {
+  if (typeof window !== "undefined") {
+    return window.location.origin;
+  }
+  return process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
+};
+
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL: getBaseURL(),
 });
+
 export const { SignIn, SignUp, SignOut, useSession } = authClient;
